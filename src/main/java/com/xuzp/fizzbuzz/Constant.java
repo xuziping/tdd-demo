e7481927890afabc086581cbc958b8878a3cc0f5
xuziping <xuzp2005@21cn.com>
2020-01-30 21:34:05 +0800

    add 2nd test case for multiple 3/5/7

---
+++ src/main/java/com/xuzp/fizzbuzz/Constant.java
@@ -0,0 +1,8 @@
+package com.xuzp.fizzbuzz;
+
+public class Constant {
+
+    public static final String FIZZ = "Fizz";
+    public static final String BUZZ = "Buzz";
+    public static final String WHIZZ = "Whizz";
+}
--- src/test/java/com/xuzp/fizzbuzz/FizzbuzzTest.java
+++ src/test/java/com/xuzp/fizzbuzz/FizzbuzzTest.java
@@ -10,4 +10,28 @@ public class FizzbuzzTest {
         FizzBuzzMain job = new FizzBuzzMain();
         Assert.assertEquals("1", job.echo(1));
     }
+
+    @Test
+    public void should_return_fizz_when_multiple_3() {
+        FizzBuzzMain job = new FizzBuzzMain();
+        Assert.assertEquals(Constant.FIZZ, job.echo(3));
+        Assert.assertEquals(Constant.FIZZ, job.echo(6));
+        Assert.assertEquals(Constant.FIZZ, job.echo(9));
+    }
+
+    @Test
+    public void should_return_buzz_when_multiple_5() {
+        FizzBuzzMain job = new FizzBuzzMain();
+        Assert.assertEquals(Constant.FIZZ, job.echo(5));
+        Assert.assertEquals(Constant.FIZZ, job.echo(10));
+        Assert.assertEquals(Constant.FIZZ, job.echo(15));
+    }
+
+    @Test
+    public void should_return_whizz_when_multiple_7() {
+        FizzBuzzMain job = new FizzBuzzMain();
+        Assert.assertEquals(Constant.FIZZ, job.echo(7));
+        Assert.assertEquals(Constant.FIZZ, job.echo(14));
+        Assert.assertEquals(Constant.FIZZ, job.echo(21));
+    }
 }
